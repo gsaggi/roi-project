b147146c14e004734e65000cfd3d54957864cf2c
gsaggi <gsaggi@gmail.com>
2024-10-31 19:35:57 +1100

    Added MainNavigator

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-import React from 'react'
-import { View, Text, Button, StyleSheet } from 'react-native'
-import { NavigationContainer } from '@react-navigation/native';
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import RootNavigator from './navigation/RootNavigator';
+import React from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import RootNavigator from "./navigation/RootNavigator";
+import MainNavigator from "./navigation/MainNavigator";
+
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-<RootNavigator />
+        {/* <RootNavigator /> */}
+        <MainNavigator />
       </NavigationContainer>
       <StatusBar style="auto" />
     </SafeAreaProvider>
   );
 }
 
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
